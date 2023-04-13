541de65ac97d7d5e33317b99805106dfe8bd2e57
Gabriel Aizman <gabaiz88@gmail.com>
2023-04-13 14:48:39 -0300

    card_car5

--- src/components/Card_Car.jsx
+++ src/components/Card_Car.jsx
@@ -3,15 +3,17 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function CarouselFadeExample() {
   return (
+    <>
+    <h1>FORD FIESTA 1.6 SE - 2017 - 41 MIL KMS</h1>
     <Carousel fade>
       <Carousel.Item className='item_car'>
         <img
           className="d-block w-100"
           src="https://firebasestorage.googleapis.com/v0/b/ventacosascasa-6149d.appspot.com/o/Fiesta%2FIMG_0861.jpg?alt=media&token=bd5f45e1-0f4f-4d45-b253-e774b85844be"
           alt="First slide"
-        />
+          />
         <Carousel.Caption>
-          <h2 className='black_letters'>FORD FIESTA 1.6 SE - 2017 - 41 MIL KMS</h2>
+          {/* <h2 className='black_letters'>FORD FIESTA 1.6 SE - 2017 - 41 MIL KMS</h2> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className='item_car'>
@@ -19,7 +21,7 @@ function CarouselFadeExample() {
           className="d-block w-100"
           src="https://firebasestorage.googleapis.com/v0/b/ventacosascasa-6149d.appspot.com/o/Fiesta%2FIMG_0862.jpg?alt=media&token=6d325870-e098-401c-9783-1a15f50d3780"
           alt="Second slide"
-        />
+          />
         <Carousel.Caption>
           {/* <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
@@ -30,12 +32,12 @@ function CarouselFadeExample() {
           className="d-block w-100"
           src="https://firebasestorage.googleapis.com/v0/b/ventacosascasa-6149d.appspot.com/o/Fiesta%2FIMG_0860.jpg?alt=media&token=4496d81b-f75d-4a3e-988a-84a099e512d6"
           alt="Third slide"
-        />
+          />
         <Carousel.Caption>
           {/* <h3>Third slide label</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p> */}
+          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+        </p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className='item_car'>
@@ -43,7 +45,7 @@ function CarouselFadeExample() {
           className="d-block w-100"
           src="https://firebasestorage.googleapis.com/v0/b/ventacosascasa-6149d.appspot.com/o/Fiesta%2FIMG_0863.jpg?alt=media&token=22144c1f-3f3d-4480-b55f-2c253b0244fb"
           alt="Four slide"
-        />
+          />
         <Carousel.Caption>
           {/* <h2 className='black_letters'>FORD FIESTA 1.6 SE - 2017 - 41 MIL KMS</h2> */}
         </Carousel.Caption>
@@ -53,7 +55,7 @@ function CarouselFadeExample() {
           className="d-block w-100"
           src="https://firebasestorage.googleapis.com/v0/b/ventacosascasa-6149d.appspot.com/o/Fiesta%2FIMG_0864.jpg?alt=media&token=3d4dc02f-63c1-4d7c-8ba0-863691682419"
           alt="Four slide"
-        />
+          />
         <Carousel.Caption>
           {/* <h2 className='black_letters'>FORD FIESTA 1.6 SE - 2017 - 41 MIL KMS</h2> */}
         </Carousel.Caption>
@@ -63,7 +65,7 @@ function CarouselFadeExample() {
           className="d-block w-100"
           src="https://firebasestorage.googleapis.com/v0/b/ventacosascasa-6149d.appspot.com/o/Fiesta%2FIMG_0868.jpg?alt=media&token=b408f72e-bede-485a-a210-6a9cb8ae87e8"
           alt="Four slide"
-        />
+          />
         <Carousel.Caption>
           {/* <h2 className='black_letters'>FORD FIESTA 1.6 SE - 2017 - 41 MIL KMS</h2> */}
         </Carousel.Caption>
@@ -73,7 +75,7 @@ function CarouselFadeExample() {
           className="d-block w-100"
           src="https://firebasestorage.googleapis.com/v0/b/ventacosascasa-6149d.appspot.com/o/Fiesta%2FIMG_0869.jpg?alt=media&token=cc8a4004-06e8-456e-9305-86dffc707177"
           alt="Four slide"
-        />
+          />
         <Carousel.Caption>
           {/* <h2 className='black_letters'>FORD FIESTA 1.6 SE - 2017 - 41 MIL KMS</h2> */}
         </Carousel.Caption>
@@ -83,7 +85,7 @@ function CarouselFadeExample() {
           className="d-block w-100"
           src="https://firebasestorage.googleapis.com/v0/b/ventacosascasa-6149d.appspot.com/o/Fiesta%2FIMG_0871.jpg?alt=media&token=660c4ec6-ba7a-425f-b728-b6e5d9650580"
           alt="Four slide"
-        />
+          />
         <Carousel.Caption>
           {/* <h2 className='black_letters'>FORD FIESTA 1.6 SE - 2017 - 41 MIL KMS</h2> */}
         </Carousel.Caption>
@@ -93,12 +95,13 @@ function CarouselFadeExample() {
           className="d-block w-100"
           src="https://firebasestorage.googleapis.com/v0/b/ventacosascasa-6149d.appspot.com/o/Fiesta%2FIMG_0873.jpg?alt=media&token=dfcc5b25-ff82-44fb-9379-86f9f5ab7ccd"
           alt="Four slide"
-        />
+          />
         <Carousel.Caption>
           {/* <h2 className='black_letters'>FORD FIESTA 1.6 SE - 2017 - 41 MIL KMS</h2> */}
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+          </>
   );
 }
 
