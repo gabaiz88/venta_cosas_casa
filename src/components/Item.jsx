2dd0fa215c85a855da18e2f8b8090a49ef1f4582
Gabriel Aizman <gabaiz88@gmail.com>
2023-04-15 19:20:32 -0300

    actualizando db

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,7 +2,15 @@ import { Button } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import format from "../functions/price_format";
 
-const Item = ({ title, descripcion, img, link, price, sold }) => {
+
+const Item = ({ title, descripcion, img, link, price }) => {
+
+  function tachado(price) {
+    if (title != "Mesa") {
+      return <span id="tachado">{price}</span>
+    }
+  }
+
   return (
     <div className="item">
       <Card style={{ width: "20em", height: "670px" }}>
@@ -11,6 +19,7 @@ const Item = ({ title, descripcion, img, link, price, sold }) => {
           <Card.Title>{title}</Card.Title>
           <Card.Text>{descripcion}</Card.Text>
           <h4 className="card_price">{format(price)}.-</h4>
+          {tachado(price)}
           <a href={link} target="blank">
             <Button variant="primary">Más Fotos..</Button>
           </a>
