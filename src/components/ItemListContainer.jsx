fcea1aae376ad8702ad5a257c47672a6982464c4
Gabriel Aizman <gabaiz88@gmail.com>
2023-04-14 23:38:23 -0300

    titulo

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,12 +1,13 @@
 import data from "../data.json";
 import ItemList from "./ItemList";
 import Card_Car from "./Card_Car";
+import TituloPagina from "./TituloPagina";
 
 const ItemListContainer = () => {
-
   return (
     <>
       <div className="catalogue">
+        <TituloPagina/>
         <Card_Car/>
         <ItemList data={data}/>
       </div>
---
+++ src/components/TituloPagina.jsx
@@ -0,0 +1,7 @@
+const TituloPagina = () => {
+  return (
+    <h1 id="text_titulo">Vendemos!!! Por cualquier duda o consulta comunicate al <br></br>11-6585-8350</h1>
+  )
+}
+
+export default TituloPagina
